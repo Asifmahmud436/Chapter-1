437d151dec864ea912357d12ecb28ae852755791
Zettex <safaandsafa4@gmail.com>
2025-01-03 15:19:35 +0600

    made the default blog model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Blog(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = 'DF','Draft'
+        PUBLISHED = 'PB','Published'
+    title = models.CharField(max_length=250)
+    slug = models.CharField(max_length=250)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='blog_posts')
+    published = models.CharField(
+        max_length=2,
+        choices=Status.choices,
+        default=Status.PUBLISHED
+        )
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-published']
+        indexes = [
+            models.Index(['-published'])
+        ]
+
+    def __str__(self):
+        return self.title
